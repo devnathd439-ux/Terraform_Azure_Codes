51e44794e1b640d7794a8efb1f4354c93f9fe99b
devnathd439.dev <dev.devnathd439@gmail.com>
2026-09-20 17:04:08 +0530

    code updated

--- project_time_machine_git/environment/dev/main.tf
+++ project_time_machine_git/environment/dev/main.tf
@@ -7,6 +7,10 @@ module "time_machine_storage_account" {
   source                       = "../../child_modules/storage_account"
   time_machine_storage_account = var.time_machine_storage_account.timemachinestorage
 }
+# module "backend_block_storage" {
+#   depends_on = [module.time_machine_storage_account]
+#   source     = "../../child_modules/backend_Block_Storage"
+# }
 module "time_machine_virtual_networks" {
   depends_on                    = [module.time_machine_rg]
   source                        = "../../child_modules/virtual_network"
